d06c6dc88c3caf4d1950d8740fd22f53e1333226
Cromega08 <cr.jrg08@gmail.com>
2025-01-08 16:07:26 -0500

    chore: add missing LICENSE
    chore: add missing LICENSE file and detail on files

--- lib/src/main/kotlin/cromega/studio/extensions/primitive/classes/AnyExtensions.kt
+++ lib/src/main/kotlin/cromega/studio/extensions/primitive/classes/AnyExtensions.kt
@@ -1,3 +1,26 @@
+/**
+ *     <Some Kotlin Utilities - Library with utility code to use on Kotlin projects>.
+ *     Copyright (C) <2025>  <Cromega>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Autor: Cromega
+ * E-mail: cr.jrg08@gmail.com
+ */
+
 package cromega.studio.extensions.primitive.classes
 
 /**
